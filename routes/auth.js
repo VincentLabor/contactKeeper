d9b5659f556c5a3a1f6e76175dbb1c3f10ef8ca7
VincentLabor <vlabor95@gmail.com>
2019-10-15 19:50:49 -0700

    Users are now able to login logically

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router(); //After calling this, we no longer use app.post or app.get but start with router. 
+const { check, validationResult } = require("express-validator");
+const jwt = require('jsonwebtoken');
+const bcrypt = require("bcryptjs");
+const config = require("config");
+const User = require('../models/User');
 
 //@route    POST  api/auth
 //@desc     Get logged in user
@@ -14,8 +19,45 @@ router.get('/', (req, res) => { //The / acts a default because from server, it w
 //@desc     Auth user and get token
 //@access   private because getting a logged in user.
 
-router.post('/', (req, res) => {
-    res.send('Log in user')
-});
+router.post('/',
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password is required').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);//This is only for routes that need validation or data
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() }); //Will give an error if something is missing or not right. 
+        }
+
+        const { email, password } = req.body;
+        try {
+            let user = await User.findOne({ email });
+            if (!user) {
+                return res.status(400).json({ msg: "Invalid credentials" })
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password); //await because this returns a promise.
+            if (!isMatch) {
+                return res.status(400).json({ msg: "Invalid credentials" })
+            }
+
+            //If the credentials are matching, we begin the process of tokenization.
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000 }, (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).json({ msg: "Server Error" });
+        }
+    });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,50 +2,62 @@ const express = require('express');
 const router = express.Router(); //After calling this, we no longer use app.post or app.get but start with router. 
 const User = require('../models/User');
 const { check, validationResult } = require("express-validator");
+const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
+const config = require("config");
 
 
 //@route    POST  api/users
 //@desc     Register a user
 //@access   Public because this is to register to become a user. 
 
-router.post('/', 
-[
-    check('name', 'Name is required').not().isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-],
- async (req, res) => {
-    const errors = validationResult(req);//This is only for routes that need validation or data
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() }); //Will give an error if something is missing or not right. 
-    }
-    
-    const {name,email,password} = req.body; //req.body should now have the name, email, and password.
-
-    try {
-        let user = await User.findOne({email}); //findOne is a mongoose command. the parameter is actually email:email but ES6 says we can just put it once. 
-        if (user){
-            res.status(400).json({msg: "user already exists"})
+router.post('/',
+    [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);//This is only for routes that need validation or data
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() }); //Will give an error if something is missing or not right. 
         }
 
-        user = new User(
-            name,
-            email,
-            password
-        ); //Need to encrypt the password before we save to the database.
+        const { name, email, password } = req.body; //req.body should now have the name, email, and password.
 
-        const salt = bcrypt.genSalt(10);
-        user.password = bcrypt.hash(password,salt); 
+        try {
+            let user = await User.findOne({ email }); //findOne is a mongoose command. the parameter is actually email:email but ES6 says we can just put it once. 
+            if (user) {
+                res.status(400).json({ msg: "user already exists" })
+            }
 
-        await user.save();
-        res.send('User saved');
-        
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send('server error'); //500 is a server error
-    }
+            user = new User({
+                name,
+                email,
+                password
+            }); //Need to encrypt the password before we save to the database.
 
-});
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000 }, (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            });
+
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('server error'); //500 is a server error
+        }
+
+    });
 
 module.exports = router;
